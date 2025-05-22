044cadf1b6613662dc07943401550a319c4ed945
Estrella Melissa Orellana Cruz <orellanaestrella66@gmail.com>
2025-05-22 09:52:02 -0600

    Método Recursivo

--- Main.java
+++ Main.java
@@ -1,3 +1,27 @@
 public class Main {
+ public static void main(String[] args) {
+    //se creo la instancia de la clase Operacion
+        Operacion op = new Operacion();
+        
+        //Mando a llamar el metodo Sumar, con diferentes tipos de datos
+        int result1 = op.Sumar(5, 10);
+        System.out.println("Resultado de la suma de enteros: " + result1);
+        
+        //Llamo al metodo Sumar con dos numeros decimales y guardo el resultado en una variable
+        double result2 = op.Sumar(5.5, 10.5);
+        System.out.println("Resultado de la suma de dobles: " + result2);
+        
+        //Metodo Sumar con tres enteros
+        //Llamo al metodo Sumar con tres enteros y guardo el resultado en una variable
+        int result3 = op.Sumar(5, 10, 15);
+        System.out.println("Resultado de la suma de tres enteros: " + result3);
+        
+        // Metodo Sumar con un numero negativo, para que de error 
+        int result4 = op.Sumar(-5, 10);
+        System.out.println("Resultado de la suma con un número negativo: " + result4);
+        
+        op.imprimirNumeroImpares(7);
+    }
 
+    
 }
--- Operacion.java
+++ Operacion.java
@@ -4,19 +4,19 @@ public class Operacion {
     // Ejemplo de sobrecarga de metodos
 
     public int Sumar(int a, int b) {
-         if(a<0||b<0)
-    {
-        System.out.println("No se puede realizar la operacion");
-        return -1;
-    }return a+b;
-      
+        if (a < 0 || b < 0) {
+            System.out.println("No se puede realizar la operacion");
+            return -1;
+        }
+        return a + b;
+
     }
 
     public double Sumar(double a, double b) {
         return a + b;
     }
 
-    public int Sumar (int a, int b, int c) {
+    public int Sumar(int a, int b, int c) {
         return a + b + c;
     }
 
@@ -24,4 +24,23 @@ public class Operacion {
         return a - b;
     }
 
+    public long Sumar(int a, long b) {
+        return a + b;
+    }
+
+    public void sumar() {
+
+    }
+
+    //Metodo recursivo para imprimir los numeros impares
+    // Se le pasa un numero entero y se imprimen los numeros impares desde ese numero hasta 0
+    public void imprimirNumeroImpares(int a){
+        if (a%2 == 1){
+        System.out.println("El numero es impar " + a);
+    }
+    a--;
+    if(a>0){
+        imprimirNumeroImpares(a);
+    }
+    }
 }
